95412029d719155a88fbd37e04e900c2228a2771
kiku72 <128595283+kiku72@users.noreply.github.com>
2023-09-13 21:52:27 +1000

    Overhauled home page layout and styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import * as userService from "../../utilities/users-service";
-import './NavBar.css'
+import "./NavBar.css";
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -22,30 +22,49 @@ export default function NavBar({ user, setUser }) {
           Log Out
         </Link>
       </nav> */}
-<div className="flex flex-wrap">
-  <section className="relative mx-auto">
-    <nav className="flex justify-between overlay-custom  text-white w-screen">
-      <div className="px-5 xl:px-6 py-2 flex w-full items-center">
-      
-          {/* <h2 className="text-4xl custom--hover custom--font">/h2> */}
-          <div className="flex justify-start w-4">
-      <h1 className="custom--font">T H E</h1>
+      <div className="flex flex-wrap overflow-x-hidden">
+        <section className="relative mx-auto">
+          <nav className="flex justify-between overlay-custom text-white w-screen">
+            <div className="px-5 xl:px-6 py-2 flex w-full items-center">
+              {/* <h2 className="text-4xl custom--hover custom--font">/h2> */}
+              <div className="flex justify-start w-4">
+                <h1 className="custom--font">T H E</h1>
+              </div>
+              <Link className="text-4xl custom--hover custom--font" to="/home">
+                Emporium
+              </Link>
+              <ul className="hidden md:flex  px-4 mx-auto  font-heading space-x-12 text-lg font-medium">
+                <li>
+                  <Link className="custom--hover custom--font text-background" to="/home">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="custom--hover custom--font text-background" to="/all-listing">
+                    All Listing
+                  </Link>
+                </li>
+                <li>
+                  <Link className="custom--hover custom--font text-background" to="/add-listing">
+                    Add Listing
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <ul className="hidden md:flex px-4 font-heading items-center space-x-12">
+              <li className="w-20">
+                <Link
+                  className="custom--hover custom--font text-background font-medium"
+                  to=""
+                  onClick={handleLogOut}
+                >
+                  Log Out
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </section>
       </div>
-          <Link className="text-4xl custom--hover custom--font" to='/home'>Emporium</Link>
-        <ul className="hidden md:flex  px-4 mx-auto  font-heading space-x-12 text-lg font-medium">
-        <li><Link className="custom--hover" to='/home'>Home</Link></li>
-        <li><Link className="custom--hover" to='/all-listing'>All Listing</Link></li>
-          <li><Link className="custom--hover" to='/add-listing'>Add Listing</Link></li>
-        </ul>
-      </div>
-      <ul className="hidden md:flex px-4 font-heading items-center space-x-12">
-      <li className="w-20">
-       <Link className="custom--hover font-medium" to='' onClick={handleLogOut}>Log Out</Link>
-      </li>
-      </ul>
-    </nav>
-  </section>
-</div>
     </>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import { getAll } from "../../utilities/listings-api";
 import "./HomePage.css";
+import Footer from "../../components/Footer/Footer"
 import ListingCard from "../../components/ListingCard/ListingCard";
 
 // needs attention...
@@ -22,36 +23,43 @@ import ListingCard from "../../components/ListingCard/ListingCard";
 export default function Home() {
   return (
     <>
-      {/* Home Page Title */}
+      <main>
+        {/* Home Page Title
       <div className="flex justify-center m-8">
         <h1 className="text-5xl font-bold">Home Page</h1>
-      </div>
+      </div> */}
 
-      {/* Latest Listing Section  */}
-      <div className="flex flex-row-reverse h-screen">
-        <div className="m-8 w-1/4 border bg-[#EAE7E7] rounded-lg shadow-md">
-          <h1 className="text-center font-semibold p-8 text-2xl">
-            Latest Listing
-          </h1>
-          <p className="m-4 custom--border-bottom">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
-            placeat beatae. Voluptate deleniti minus vel quam quis quae cum,
-            repellendus, eaque officiis sed distinctio earum quod excepturi
-            voluptatibus. Architecto, repellendus.
-          </p>
-        </div>
-        {/* Search Bar */}
-        <div className="absolute custom--positon">
-          <div class="m-8 w-2/5 h-16 bg-[#EAE7E7] rounded-lg shadow-md  space-x-4">
+        <div className="grid gap-y-3 gap-x-0 h-screen w-screen">
+          {/* Searchbar */}
+          <div className="searchbar h-1/1 bg-[#f7f2f2] rounded-lg shadow-md text-center">
             <input
-              className="bg-[#ffe8d1] rounded-lg w-1/2 m-2"
+              className="bg-background rounded-lg xl:w-5/6 w-1/2"
               type="text"
-              placeholder="search Listing..."
+              placeholder="search listings..."
             />
-            <button className="bg-[#ff9041] m-3 space-x-4">Search</button>
+            <button className="bg-[#ff9041] m-3 space-x-4 custom-landing-font">Search</button>
           </div>
+
+          {/* Sidebar */}
+          <div className="sidebar bg-secondary rounded-lg ml-4 mr-8">
+            <h3 className="text-3xl text-left text-white ml-10 mt-5 custom--font">Newest Listings</h3>
+          </div>
+          
+          <section className="content-one bg-primary rounded-lg ml-8">
+            <h2 className="text-3xl text-left text-white ml-10 mt-5 custom--font">Trading Cards</h2>
+          </section>
+          <section className="content-two bg-[#f7f2f2] rounded-lg ml-8">
+            <h2 className="text-3xl text-left text-white ml-10 mt-5 custom--font">Memorabilia</h2>
+          </section>
+          <section className="content-three bg-[#f7f2f2] rounded-lg ml-8">
+            <h2 className="text-3xl text-left text-white ml-10 mt-5 custom--font">Collectible</h2>
+          </section>
+          <section className="content-four bg-[#f7f2f2] rounded-lg ml-8">
+            <h2 className="text-3xl text-left text-white ml-10 mt-5 custom--font">Other</h2>
+          </section>
+          <Footer />
         </div>
-      </div>
+      </main>
 
       {/* <div>
         {listings.length === 0 ? (
